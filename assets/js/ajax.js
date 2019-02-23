f3ee0f6fc2563091b6e233f03ea1a2939e4cb4f1
JKRyan <jed@jedkendall.com>
2019-02-23 12:22:16 -0600

    Initial setup and commits

---
+++ assets/js/ajax.js
@@ -0,0 +1,10 @@
+function getGif() {
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
+
+};
---
+++ assets/js/globals.js
@@ -0,0 +1,7 @@
+//Load first
+
+var fTopics = ["space", "monster hunter", "video games", "warhammer", "dark souls", "comedy", "super mario", "doom", "memes", "cats", "samoyed", "corgi", "fail", "raccoons"];
+
+
+
+var queryURL = "";
